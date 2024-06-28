945b64559aec610ed476cfeefc34815e000f954e
Confidence Emonena <confidinho@yahoo.com>
2024-06-29 00:06:10 +0100

    video player

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -35,7 +35,7 @@ const RootLayout = () => {
   }
   return (
     <GlobalProvider>
-      <Stack>
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
